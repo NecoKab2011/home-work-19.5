9c47718ddecdc728dcada837ff321ac11c7ecc9e
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2025-11-02 10:14:05 +0100

    Updates

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonLM } from "./Button"
-
-export default function Button({ onClick }) {
-  return (
-    <ButtonLM type="button" onClick={onClick}>
-      Load more
-    </ButtonLM>
-  );
-}
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-import { ImageGalleryItems, ImageGalleryItemImage } from "./ImageGalleryItem"
-
-export default function ImageGalleryItem({ small, large, onClick }) {
-  return (
-    <ImageGalleryItems onClick={() => onClick(large)}>
-      <ImageGalleryItemImage src={small} alt="" />
-    </ImageGalleryItems>
-  );
-}
